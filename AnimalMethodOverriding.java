05a3d4142a5cafcc0407b3f3fae8eeced51d95ff
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-06 01:49:00 +0530

    Update AnimalMethodOverriding.java

--- AnimalMethodOverriding.java
+++ AnimalMethodOverriding.java
@@ -1,1 +1,20 @@
+class Animal {
+    public void eat() {
+        System.out.println("The animal is eating.");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void eat() {  // Method overriding
+        System.out.println("The dog is eating dog food.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();  // Calls the overridden method in Dog
+    }
+}
 
