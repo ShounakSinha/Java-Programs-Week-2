40e8fd23bbdfc4b197a9a947c42b45faf4948875
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-06 01:47:16 +0530

    Update Fruitsinheritance.java

--- Fruitsinheritance.java
+++ Fruitsinheritance.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
 
+class Fruit {
+    private String fruitName = "";
+    private int fruitWeight = 0;
+
+    void setFruitName(String n) {
+        fruitName = n;
+    }
+
+    String getFruitName() {
+        return fruitName;
+    }
+
+    void setFruitWeight(int w) {
+        fruitWeight = w;
+    }
+
+    int getFruitWeight() {
+        return fruitWeight;
+    }
+
+}
+
+// inheritance
+class Mango extends Fruit {
+    private String mangoType = "";
+
+    void setMangoType(String t) {
+        mangoType = t;
+    }
+
+    String getMangoType() {
+        return mangoType;
+    }
+}
