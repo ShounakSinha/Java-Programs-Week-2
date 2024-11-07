3e6e4cf4d977a0d7446e51839ba39c94abd5b092
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-08 00:27:03 +0530

    Update overloadedconstructorcode.java

--- overloadedconstructorcode.java
+++ overloadedconstructorcode.java
@@ -1,1 +1,16 @@
+class baseClass{
+    baseClass(){
+        System.out.println("Ami base class");
+    }
+}
 
+class derivedClass extends baseClass{
+    derivedClass
+}
+
+
+class overloadedconstructorcode{
+    //baseClass bC= new naseClass();
+    derivedClass dC= new derivedClass();
+    
+}
