34bde4a135854bf3860f54d5841c40d9448fe997
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-08 00:36:11 +0530

    Update overloadedconstructorcode.java

--- overloadedconstructorcode.java
+++ overloadedconstructorcode.java
@@ -1,13 +1,23 @@
-class baseClass{
-    baseClass(){
+class baseClass {
+    baseClass() {
         System.out.println("Ami base class");
+    }
+
+    baseClass(int x) {
+        System.out.println("Ami base class with x");
     }
 }
 
-class derivedClass extends baseClass{
-    derivedClass
-}
+class derivedClass extends baseClass {
+    derivedClass() {
+        super(10);
+        System.out.println("Eta derived class");
+    }
 
+    derivedClass(int x) {
+        System.out.println("Eta derived class with x");
+    }
+}
 
 class overloadedconstructorcode {
     public static void main(String args[]) {
