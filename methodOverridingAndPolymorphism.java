c85c3f23e186a781ca495b6af6ac5d9ac66e6115
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-09 01:09:19 +0530

    Update methodOverridingAndPolymorphism.java

--- methodOverridingAndPolymorphism.java
+++ methodOverridingAndPolymorphism.java
@@ -1,1 +1,32 @@
+class BankAccount {
+    public void calculateInterest() {
+        System.out.println("Calculating interest at base rate.");
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    @Override
+    public void calculateInterest() {
+        System.out.println("Calculating interest at savings account rate.");
+    }
+}
+
+class CheckingAccount extends BankAccount {
+    @Override
+    public void calculateInterest() {
+        System.out.println("Calculating interest at checking account rate.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount baseAccount = new BankAccount();
+        BankAccount savings = new SavingsAccount();
+        BankAccount checking = new CheckingAccount();
+
+        baseAccount.calculateInterest(); // Base rate interest
+        savings.calculateInterest();     // Savings rate interest
+        checking.calculateInterest();    // Checking rate interest
+    }
+}
 
