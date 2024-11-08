321013eba988360dfd7b7f9a6315a5a45008c9d3
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-09 00:45:51 +0530

    Update superpractise.java

--- superpractise.java
+++ superpractise.java
@@ -1,1 +1,26 @@
+class Animal {
+    public void eat() {
+        System.out.println("The animal is eating.");
+    }
+    public void eat(String food) {
+        System.out.println("The animal is eating " + food + ".");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void eat() {
+        //super.eat("Mutton"); // Calls the superclass (Animal's) eat method
+        System.out.println("The dog is eating dog food.");
+        // super.eat("Chicken");
+        // super.eat();
+    }
+}
+
+public class animalinheritance_methodoverriding {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+    }
+}
 
