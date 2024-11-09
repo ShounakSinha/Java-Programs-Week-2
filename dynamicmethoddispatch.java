4dac5563d53277936aec0a62a0bb1755929179cc
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-10 00:58:02 +0530

    Update dynamicmethoddispatch.java

--- dynamicmethoddispatch.java
+++ dynamicmethoddispatch.java
@@ -1,1 +1,15 @@
+public class dynamicmethoddispatch {
+    public static void main(String[] args) {
+        // A a = new A();
+        // B b = new B();
+        // C c = new C();
+        // A r;
+        // r = a;
+        // r.callme();
+        // r = b;
+        // r.callme();
+        // r = c;
+        // r.callme();
+
+        //this code snippet below is also a demonstration of dynamic method dispatch
 
