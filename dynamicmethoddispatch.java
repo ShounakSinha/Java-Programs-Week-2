dc4021753402f4f84b99ea43b69ac258949b7063
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-10 01:04:36 +0530

    Update dynamicmethoddispatch.java

--- dynamicmethoddispatch.java
+++ dynamicmethoddispatch.java
@@ -1,3 +1,15 @@
+class A {
+    void callme() {
+        System.out.println("Inside A's callme method");
+    }
+}
+
+class B extends A {
+    void callme() {
+        System.out.println("Inside B's callme method");
+    }
+}
+
 public class dynamicmethoddispatch {
     public static void main(String[] args) {
         // A a = new A();
