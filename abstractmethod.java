b6bfdc73846b7ed487df0bb5008c3a64c07cad95
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-12 01:02:53 +0530

    Update abstractmethod.java

--- abstractmethod.java
+++ abstractmethod.java
@@ -1,1 +1,13 @@
+abstract class Parent2 {
+    public Parent2() {
+        System.out.println("Mai base2 ka constructor hoon");
+    }
 
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    abstract public void greet();
+
+    abstract public void greet2();
+}
